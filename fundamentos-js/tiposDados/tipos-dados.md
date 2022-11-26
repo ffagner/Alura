4694bd6af2b61b6441e15d162dda13dc8daea3b8
Fagner Farias <60819320+ffagner@users.noreply.github.com>
2022-11-25 23:34:50 -0300

    estudo de tipos de dados no javascript usando nodejs

---
+++ fundamentos-js/tiposDados/tipos-dados.md
@@ -0,0 +1,16 @@
+# Tipos de Dados
+Tipos de dados são valores que podem ser armazenados em variáveis.
+
+## variáveis
+Variáveis são espaços na memória do computador que podem ser usados para armazenar valores. Para criar uma variável, você deve usar a palavra-chave `var` seguida pelo nome da variável. Por exemplo:
+
+Os tipos de dados em javascript podem ser divididos em 2 grupos: primitivos e objetos.
+
+Os tipos primitivos são: `string`, `number`, `boolean`, `null` e `undefined`.
+
+
+## Tipo: Number
+O tipo number é um tipo numérico, que pode ser inteiro ou decimal.
+
+## Tipo: String
+O tipo string é um tipo de texto, que pode ser representado por aspas simples ou duplas.
---
+++ fundamentos-js/tiposDados/tiposDados.js
@@ -0,0 +1,50 @@
+// tipos de dados number
+
+//tipos numéricos possiveis
+let num = 3
+const num2 = 3.5
+const num3 = 3.1415
+const num4 = 3
+const num5 = .5
+
+//operações matemáticas	
+//soma
+console.log(num + num2)
+
+//subtração
+console.log(num - num3)
+
+//multiplicação
+console.log(num3 * num2)
+
+//divisão
+console.log(num4 / num)
+
+//exponenciação
+console.log(num ** num2)
+
+//módulo
+console.log(num % num2)
+
+//incremento
+console.log(num++)
+
+//decremento
+console.log(num--)
+
+//NaN - Not a Number
+console.log(7 / 'oi')
+
+// ================================================================
+
+//tipos de dados string
+
+//string
+const nome = 'Fagner'
+const sobrenome = 'Farias'
+const nomeCompleto = nome + ' ' + sobrenome
+console.log(nomeCompleto)
+
+//=================================================================
+
+
